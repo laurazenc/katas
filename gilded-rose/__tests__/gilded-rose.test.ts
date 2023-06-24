d46cff5a8419cacebefcdc621bc209a33f21e3fa
laurazenc <laurazenc@gmail.com>
2023-06-24 18:57:08 +0200

    test(gilded-rose): add approvals and pass test

--- gilded-rose/__tests__/gilded-rose.test.ts
+++ gilded-rose/__tests__/gilded-rose.test.ts
@@ -1,9 +1,11 @@
+import { verify } from "approvals/lib/Providers/Jest/JestApprovals";
+
 import { GildedRose, Item } from "../src/gilded-rose.ts";
 
 describe("Gilded Rose", () => {
 	it("should foo", () => {
 		const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
 		const items = gildedRose.updateQuality();
-		expect(items[0].name).toBe("fixme");
+		verify(items[0].name);
 	});
 });
