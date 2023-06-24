e7508f8774ef8c79dc2c65637ee1c9c15a93e438
laurazenc <laurazenc@gmail.com>
2023-06-24 19:42:18 +0200

    test(gilded-rose): full covered

--- gilded-rose/__tests__/gilded-rose.test.ts
+++ gilded-rose/__tests__/gilded-rose.test.ts
@@ -1,11 +1,24 @@
-import { verify } from "approvals/lib/Providers/Jest/JestApprovals";
+import { verifyAllCombinations3 } from "approvals/lib/Providers/Jest/CombinationApprovals";
 
 import { GildedRose, Item } from "../src/gilded-rose.ts";
 
+function calculateQuality(name: string, quality: number, sellIn: number) {
+	const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+	const result = gildedRose.updateQuality();
+
+	return JSON.stringify(result[0]);
+}
+
 describe("Gilded Rose", () => {
-	it("should foo", () => {
-		const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
-		const items = gildedRose.updateQuality();
-		verify(items[0].name);
+	it("should calculate quality", () => {
+		const names = [
+			"foo",
+			"Sulfuras, Hand of Ragnaros",
+			"Aged Brie",
+			"Backstage passes to a TAFKAL80ETC concert",
+		];
+		const qualities = [0, 1, 2];
+		const sellIns = [-1, 0, 1];
+		verifyAllCombinations3(calculateQuality, names, qualities, sellIns);
 	});
 });
